af7da04f4affca3105b97feb652d6a2e9b66c6a3
Vjfrib <107771171+Vjfrib@users.noreply.github.com>
2024-01-04 21:05:16 +0000

    ERA PRA PEGAR DAQUI

--- Main.java
+++ Main.java
@@ -1,14 +1,47 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Scanner;
 
-// import org.junit.jupiter.api.Test;
+//Endereço: typedef de uma estrutura heterogenea = classe de atributos (apenas).
+public class Endereço {
+  private String rua;
+  private int numeroCasa;
+  private int cep;
+}
+
+
+public class Cliente{
+  private String cpf;
+  private String nome;
+  private double salario;
+  //private Endereco endereco;
+
+  // Construtor que recebe cpf e nome como parâmetros
+  public Cliente(String nomeRecebido, int cpfRecebido) {
+      this.cpf = cpfRecebido + "1";
+      this.nome = nomeRecebido;
+  }
+
+  // Método para calcular o valor total do rendimento em um período
+
+}
+
 
 class Main {
   public static void main(String[] args) {
-    System.out.println("Gott Mit Uns");
-  }
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+    Scanner scr = new Scanner(System.in);
+
+    //passar os valores para os atributos dos obj.
+    System.out.print("Digite o nome do cliente: ");
+    String inputarNome = scr.nextLine();
+
+    System.out.print("Digite o CPF do cliente: ");
+    int inputarCPF = Integer.parseInt(scr.next());
+
+    //Debug
+    //System.out.println(inputarNome + "\n" + inputarCPF);
+
+    Cliente cliente1 = new Cliente(inputarNome, inputarCPF);
+    //cliente1.calcularRendimento(10);
+
+  }
 }
